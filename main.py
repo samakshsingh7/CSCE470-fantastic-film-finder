92bba17dfe65879b6551b8937c97b4e6cbbbc616
Hoang Nghia Anh <46215518+hnathehe2@users.noreply.github.com>
2022-03-28 18:47:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-import pandas as pd 
-pd.options.display.width = 0
+import pandas as pd
+from decimal import Decimal
 
 
-name_basics_df = pd.read_csv('title_basics_data.tsv', sep='\t', dtype=str, nrows=5000)
-title_basics_df = pd.read_csv('title_rating_data.tsv', sep='\t', dtype=str, nrows=5000)
+basics_df = pd.read_csv('title_basics_data.tsv', sep='\t', dtype=str, nrows=10000)
+basics_df['genres'] = basics_df['genres'].str.split(',')
 
+rating_df = pd.read_csv('title_rating_data.tsv', sep='\t', dtype=str, nrows=10000)
+movie_query = "John"
 
+title_rating_df = pd.merge(left=basics_df, right=rating_df, left_on='tconst',right_on='tconst')
 
-# title = title_akas_df[title_akas_df["region"]=="US"]
-# title_basics_df = title_basics_df[title_basics_df["startYear"] > 2000]
+movie_result = title_rating_df.loc[title_rating_df['primaryTitle'].str.contains(movie_query, case=False)]
+movie_result = movie_result.sort_values(by=['averageRating'], ascending=False)
 
+print(movie_result)
 
-print(name_basics_df)
-
-title_merged = pd.merge(left=name_basics_df, right=title_basics_df, left_on='tconst',right_on='tconst')
-# title_merged = title_merged[title_merged['titleType']=="movie"]
-# title_merged = pd.merge(left=title_merged, right = title_rating_df, left_on='titleId', right_on='tconst')
-# title_merged = pd.merge(left=title_merged, right = title_crew_df, left_on='titleId', right_on='tconst1')
-# title_merged=title_merged.drop(labels=['numVotes','tconst','tconst1','ordering','language','types','attributes','isOriginalTitle','titleType','primaryTitle','originalTitle','isAdult','endYear'],axis=1)
-
-#Converts title_merged df into dictionary (Format --> {movieName : {'titleId' : ..., 'Year': ..., etc.)
-title_dict = {title_merged['primaryTitle'][index]:{} for index in title_merged.index}
-
-for index in title_merged.index:
-    movieName = title_merged['primaryTitle'][index]
-    title_dict[movieName] = {'primaryTitle' : title_merged['primaryTitle'][index],'Year' : title_merged['startYear'][index],'Runtime' : title_merged['runtimeMinutes'][index], 'Rating' : title_merged['averageRating'][index], 'Genre' : title_merged['genres'][index]}
-
-
-count = 0
-for i in title_dict:
-    count += 1
-    if count < 10:
-        print(i, title_dict[i])
+# We are currently testing our searching to not only find name including the query word but also related to the query word
+# Such as Jon, Jonny, Johny, Jojo, ... and insert that into the name searching
