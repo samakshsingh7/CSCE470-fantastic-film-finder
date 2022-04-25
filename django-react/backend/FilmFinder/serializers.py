2321936ee7a4418ef76c3dbbdea3bd932026ab71
josephgleal <jleal2018@gmail.com>
2022-04-25 04:28:34 -0500

    delete files

--- django-react/backend/FilmFinder/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Film
-
-class FilmSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Film
-        fields = ('id', 'title', 'description')
